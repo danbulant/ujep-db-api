c72dc4f36060edce90a98f05babd1ba4c8d94260
Daniel Bulant <danbulant@gmail.com>
2022-06-16 21:02:33 +0200

    more user tests

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,11 +1,11 @@
 import request from "supertest";
-import { app } from "../app";
-import { jest } from '@jest/globals';
-import { Place } from "../models/place";
-import { User } from "../models/user";
 import mongoose from "mongoose";
 import bcrypt from 'bcrypt';
 import { SignJWT } from "jose";
+
+import { app } from "../app";
+import { Place } from "../models/place";
+import { User } from "../models/user";
 import { privateKey } from "../keys";
 
 /** @type {Place} */
@@ -106,16 +106,44 @@ test("Logging in with correct details", async () => {
     expect(res.status).toBe(200);
 });
 
+test("Non-existent user token fails", async () => {
+    const TOKEN = await new SignJWT({ 'sub': PLACE.id })
+        .setProtectedHeader({ alg: 'ES256' })
+        .setIssuedAt()
+        .setIssuer('urn:pomuckydb:issuer')
+        .setAudience('urn:pomuckydb:audience')
+        .setExpirationTime('12h')
+        .sign(privateKey);
+    console.log(TOKEN);
+    const res = await request(app.callback()).get("/users/@self").set("Cookie", `token=${TOKEN}`);
+    expect(res.status).toBe(403);
+    expect(res.body.message).toBe("user_deleted");
+});
+
+test("Updating place without user fails", async () => {
+    const res = await request(app.callback()).put(`/users/@self/place`).send({
+        name: "new name"
+    });
+    expect(res.status).toBe(401);
+});
+
+test("Getting place with invalid ID fails", async () => {
+    const res = await request(app.callback()).get(`/places/0`);
+    expect(res.status).toBe(404);
+});
+test("Getting non-existent place fails", async () => {
+    const res = await request(app.callback()).get(`/places/000000000000000000000000`);
+    expect(res.status).toBe(404);
+});
+
 //! This must be ran always all at once. Other routes will not work until password is changed
 describe("Developer User operations", () => {
-    /// TODO: Fix this test
-    /// Jest runs code parallely, so this may not always fail
-    // test("Getting current user details fails as password change is required", async () => {
-    //     const res = await request(app.callback())
-    //         .get("/users/@self")
-    //         .set('Cookie', `token=${JWT}`)
-    //     expect(res.statusCode).toBe(401);
-    // });
+    test("Getting current user details fails as password change is required", async () => {
+        const res = await request(app.callback())
+            .get("/users/@self")
+            .set('Cookie', `token=${USER_JWT}`)
+        expect(res.statusCode).toBe(403);
+    });
     test("Changing password", async () => {
         const res = await request(app.callback())
             .put("/users/@self")
@@ -145,7 +173,6 @@ describe("Developer User operations", () => {
         const res = await request(app.callback())
             .get("/users/@self/place")
             .set('Cookie', `token=${USER_JWT}`);
-        console.log(res.body);
         expect(res.statusCode).toBe(200);
     });
     test("Getting list of users in place returns 1 user", async () => {
@@ -155,11 +182,95 @@ describe("Developer User operations", () => {
         expect(res.statusCode).toBe(200);
         expect(res.body.length).toBe(1);
     });
+    test("Getting list of all users returns 1 user", async () => {
+        const res = await request(app.callback())
+            .get("/users")
+            .set('Cookie', `token=${USER_JWT}`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.length).toBe(1);
+    });
     test("Getting user details by id", async () => {
         const res = await request(app.callback())
             .get(`/users/${USER._id}`)
             .set('Cookie', `token=${USER_JWT}`);
         expect(res.statusCode).toBe(200);
+    });
+    // TODO: Correctly implement place updates
+    test("Updating place gives internal error", async () => {
+        const res = await request(app.callback())
+            .put(`/users/@self/place`)
+            .set('Cookie', `token=${USER_JWT}`);
+        expect(res.statusCode).toBe(500);
+    });
+    describe("Creating user", () => {
+        let newUserID;
+        afterAll(async () => {
+            if(newUserID) {
+                await User.deleteOne({ _id: newUserID });
+            }
+        });
+        test("Creating user", async () => {
+            const res = await request(app.callback())
+                .post("/users")
+                .set('Cookie', `token=${USER_JWT}`)
+                .send({
+                    name: "new@example.com",
+                    password: "newpassword",
+                    role: 1
+                });
+            expect(res.statusCode).toBe(200);
+        });
+        test("Creating user with invalid body fails", async () => {
+            const res = await request(app.callback())
+                .post("/users")
+                .set('Cookie', `token=${USER_JWT}`)
+                .send("");
+            expect(res.statusCode).toBe(400);
+        });
+        test("Creating user with invalid role format fails", async () => {
+            const res = await request(app.callback())
+                .post("/users")
+                .set('Cookie', `token=${USER_JWT}`)
+                .send({
+                    name: "test@example.com",
+                    password: "newpassword",
+                    role: "bad"
+                });
+            expect(res.statusCode).toBe(400);
+        });
+    });
+    describe("Creating user in another place", () => {
+        let place;
+        let userId;
+        beforeAll(async () => {
+            place = await Place.create({
+                name: "another test place",
+                description: "test description2",
+                contacts: []
+            });
+        });
+        afterAll(async () => {
+            if(userId) {
+                await User.deleteOne({ _id: userId });
+            }
+            await Place.deleteOne({ _id: place._id });
+        });
+        test("Create user in another place", async () => {
+            const res = await request(app.callback())
+                .post("/users")
+                .set('Cookie', `token=${USER_JWT}`)
+                .send({
+                    name: "placed@example.com",
+                    password: "newpassword",
+                    role: 1,
+                    place: place._id
+                });
+            expect(res.statusCode).toBe(200);
+            expect(res.body.name).toBe("placed@example.com");
+            expect(res.body.role).toBe(1);
+            expect(res.body.place).toBe(place.id);
+            userId = res.body._id;
+        });
     });
 });
 
@@ -214,5 +325,52 @@ describe("Using DEFAULT user", () => {
             .set('Cookie', `token=${USER_JWT}`);
         expect(res.statusCode).toBe(200);
         expect(res.body.name).toBe("test place");
+    });
+});
+
+describe("Using LOCAL_ADMIN user", () => {
+    beforeAll(async () => {
+        USER.role = 3;
+        await USER.save();
+    });
+    afterAll(async () => {
+        USER.role = 5;
+        await USER.save();
+    });
+    test("Getting current user details", async () => {
+        const res = await request(app.callback())
+            .get("/users/@self")
+            .set('Cookie', `token=${USER_JWT}`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.role).toBe(3);
+    });
+    test("Getting list of all users fails", async () => {
+        const res = await request(app.callback())
+            .get("/users")
+            .set('Cookie', `token=${USER_JWT}`);
+        expect(res.statusCode).toBe(403);
+    });
+    test("Creating new user in another place fails", async () => {
+        const res = await request(app.callback())
+            .post("/users")
+            .set('Cookie', `token=${USER_JWT}`)
+            .send({
+                name: "test@example.com",
+                password: "newpassword",
+                role: 3,
+                place: "5e9f8f8f8f8f8f8f8f8f8f8"
+            });
+        expect(res.statusCode).toBe(403);
+    });
+    test("Creating user with higher role fails", async () => {
+        const res = await request(app.callback())
+            .post("/users")
+            .set('Cookie', `token=${USER_JWT}`)
+            .send({
+                name: "test@example.com",
+                password: "newpassword",
+                role: 4
+            });
+        expect(res.statusCode).toBe(403);
     });
 });
